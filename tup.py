010020630525bc08add6799b940a6c866f213404
Dhruv134 <55887431+Dhruv134@users.noreply.github.com>
2020-07-10 09:59:10 +0530

    first commit

---
+++ tup.py
@@ -0,0 +1,29 @@
+t = (1,2,2,4,5)
+l = [1,2,2,4,5]
+lan = ('c', 'c++', 'Java', 'Python', 'R')
+# print(lan[3])
+
+# print(lan[1:4])
+
+# mix = t+lan
+
+# print(mix)
+
+# List Vs. Tuple
+# l[2]=3
+# print("This Is List : ",l)
+#
+# t[2]=3
+# print("This Is Tuple : ",t)
+
+# Function
+
+t = (1,2,5,4,3)
+l = [1,2,3,4,5]
+
+# print(len(t))
+
+# print(min(t))
+
+# t1 = tuple(l)
+# print(type(t1))
