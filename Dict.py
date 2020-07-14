6865cdbb23db32436b03223c84d6d76aa9e29fa6
Dhruv134 <55887431+Dhruv134@users.noreply.github.com>
2020-07-14 10:25:27 +0530

    Dict commit

---
+++ Dict.py
@@ -0,0 +1,17 @@
+# l = [1,2,3,4,5]
+# d = {1:"A", 2:"B",3:"c"}
+
+# print(l[2])
+# d[4] = "D"
+# print(d[4])
+
+# dict = {'no':1,'name':'Hard Coder', 'Dep':'Computer Sci'}
+# dict['Dep'] = 'Information Techno'
+# dict['Dep'] = ['Computer Sci','Information Techno']
+# print(dict)
+
+# dict1 = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+# del dict1['Name']
+# dict1.clear()
+# del dict1
+# print(dict1)
